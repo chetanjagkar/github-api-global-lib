3f019b251a46cd083843f1e0f2dc845af339aabe
chetanjagkar <chetanjagkar@gmail.com>
2023-12-12 22:08:56 +0000

    Create PipelineUtility.groovy

---
+++ src/PipelineUtility.groovy
@@ -0,0 +1,21 @@
+class PipelineUtility
+{
+
+
+  def markBuildStatus(script,pipelineStage,err,pipelinGitlabState,currentBuildResult)
+  {
+      script.echo "Build failed at ${pipelineStage} stage with Error ${err}"
+      if("${err}" == "org.jenkinsci.plugins.workflow.steps.FlowInterruptedException")
+      {
+          script.updateGitlabCommitStatus name: "${pipelineStage}", state: 'canceled'
+          script.currentBuild.result = 'ABORTED'
+      }
+      else
+      {
+          script.updateGitlabCommitStatus name: "${pipelineStage}", state: "${pipelinGitlabState.toLowerCase()}"
+          script.currentBuild.result = "${currentBuildResult.toUpperCase()}"
+          script.error "Build failed at ${pipelineStage} stage with Error ${err}"
+          script.sh 'exit 1'
+      }
+  }
+}
